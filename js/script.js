9b06bf83d455fb33e0472ad31beab12b604099e1
andreasdelu <Andreasdelu@gmail.com>
2022-01-31 18:15:17 +0100

    order

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const aflever = document.getElementById("aflevering");
 const personer = document.getElementById("personer");
 const kufferter = document.getElementById("kufferter");
 
-const output = document.getElementById("biler");
+
 const skabelon = document.getElementById("skabelon");
 
 const biler = [
@@ -14,7 +14,8 @@ const biler = [
         kategori: "Budget",
         personer: 4,
         kufferter: 0,
-        pris: "799,00"
+        pris: "799,00",
+        nummer: 0
     },
     {
         billede: "images/biler/3.png",
@@ -23,7 +24,8 @@ const biler = [
         kategori: "Mellemklasse",
         personer: 5,
         kufferter: 3,
-        pris: "999,00"
+        pris: "999,00",
+        nummer: 1
     },
     {
         billede: "images/biler/touran.png",
@@ -32,7 +34,8 @@ const biler = [
         kategori: "Minivan",
         personer: 7,
         kufferter: 4,
-        pris: "1099,00"
+        pris: "1099,00",
+        nummer: 2
     }
 ];
 
@@ -49,42 +52,63 @@ if (day <=9) {
     day = "0" + day;
 }
 
-afhent.value = `${year}-${month}-${day}`;
-aflever.value = `${year}-${month}-${day}`;
-afhent.min = afhent.value;
-aflever.min = afhent.value;
+dataStorage = window.sessionStorage;
 
-function search() {
-    output.innerHTML = '';
-    for (const bil of biler) {
-        if (bil.personer >= personer.value && bil.kufferter >= kufferter.value) {
-            const klon = skabelon.content.cloneNode(true);
-            const billede = klon.querySelector(".billede");
-            const model = klon.querySelector(".model");
-            const brand = klon.querySelector(".brand");
-            const kategori = klon.querySelector(".bil-kategori");
-            const bilpersoner = klon.querySelector(".bil-personer");
-            const bilkufferter = klon.querySelector(".bil-kufferter");
-            const pris = klon.querySelector(".pris");
+if (window.location.pathname === '/index.html') {
 
-            billede.src = bil.billede;
-            model.textContent += bil.model;
-            brand.textContent += bil.brand;
-            kategori.textContent += bil.kategori;
-            bilpersoner.textContent += bil.personer;
-            bilkufferter.textContent += bil.kufferter;
-            pris.textContent += bil.pris;
-            output.appendChild(klon);
+    afhent.value = `${year}-${month}-${day}`;
+    aflever.value = `${year}-${month}-${day}`;
+    afhent.min = afhent.value;
+    aflever.min = afhent.value;
+
+    function search() {
+        const output = document.getElementById("biler"); 
+        output.innerHTML = '';
+        for (const bil of biler) {
+            if (bil.personer >= personer.value && bil.kufferter >= kufferter.value) {
+                const klon = skabelon.content.cloneNode(true);
+                const billede = klon.querySelector(".billede");
+                const model = klon.querySelector(".model");
+                const brand = klon.querySelector(".brand");
+                const kategori = klon.querySelector(".bil-kategori");
+                const bilpersoner = klon.querySelector(".bil-personer");
+                const bilkufferter = klon.querySelector(".bil-kufferter");
+                const pris = klon.querySelector(".pris");
+                
+                const bookbtn = klon.querySelector(".booknu");
+                bookbtn.onclick = function(){bookNu(bil.nummer);};
+
+                billede.src = bil.billede;
+                model.textContent += bil.model;
+                brand.textContent += bil.brand;
+                kategori.textContent += bil.kategori;
+                bilpersoner.textContent += bil.personer;
+                bilkufferter.textContent += bil.kufferter;
+                pris.textContent += bil.pris;
+                output.appendChild(klon);
+            }
+        }
+        if (output.innerHTML == '') {
+            output.insertAdjacentHTML("afterbegin", '<h2>Ingen biler fundet :(</h2>')
         }
     }
-    if (output.innerHTML == '') {
-        output.insertAdjacentHTML("afterbegin", '<h2>Ingen biler fundet :(</h2>')
+    search();  
+
+    function bookNu(order) {
+        dataStorage.setItem("ordernum", order);
+        dataStorage.setItem("afhent", afhent.value);
+        dataStorage.setItem("aflever", aflever.value);
+
+        window.location.href = "/order.html";
     }
 }
 
-function loadBiler() {
-    output.innerHTML = '';
-    for (const bil of biler) {
+if (window.location.pathname === '/order.html') {
+
+    const vogn = document.getElementById("cart");
+    function fillCart() {
+        var ordernum = dataStorage.getItem("ordernum");
+        ordernum = ordernum.replace("ordernum=", "");
         const klon = skabelon.content.cloneNode(true);
         const billede = klon.querySelector(".billede");
         const model = klon.querySelector(".model");
@@ -92,16 +116,20 @@ function loadBiler() {
         const kategori = klon.querySelector(".bil-kategori");
         const bilpersoner = klon.querySelector(".bil-personer");
         const bilkufferter = klon.querySelector(".bil-kufferter");
-        const pris = klon.querySelector(".pris");
+    
+        billede.src = biler[ordernum].billede;
+        model.textContent += biler[ordernum].model;
+        brand.textContent += biler[ordernum].brand;
+        kategori.textContent += biler[ordernum].kategori;
+        bilpersoner.textContent += biler[ordernum].personer;
+        bilkufferter.textContent += biler[ordernum].kufferter;
 
-        billede.src = bil.billede;
-        model.textContent += bil.model;
-        brand.textContent += bil.brand;
-        kategori.textContent += bil.kategori;
-        bilpersoner.textContent += bil.personer;
-        bilkufferter.textContent += bil.kufferter;
-        pris.textContent += bil.pris;
-        output.appendChild(klon);
+        vogn.appendChild(klon);
+        vogn.insertAdjacentHTML("beforeend", biler[ordernum].pris + "<br>");
+        vogn.insertAdjacentHTML("beforeend", dataStorage.getItem("afhent") + "<br>");
+        vogn.insertAdjacentHTML("beforeend", dataStorage.getItem("aflever"));
     }
+    fillCart();
 }
-loadBiler();
+
+
